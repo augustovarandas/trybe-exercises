c62b2d01f77b8f19a7d8a93af73668a522797f26
augustovarandas <augustovarandas21@gmail.com>
2022-02-02 17:44:42 -0300

    Adicionado exercicio fixação switch e case

---
+++ fundamentos/bloco-04-javascript-e-programacao-de-logica/dia-4.01-javascript-primeiros-passos/4.01.5-switch-e-case/switch-case.js
@@ -0,0 +1,15 @@
+let resultado = "reprovada";
+
+switch (resultado) {
+    case "aprovada":
+        console.log ("Parabéns, você foi aprovada!");
+        break;
+
+    case "lista":
+        console.log ("Legal, você está em nossa lista de espera!")
+        break;
+
+    case "reprovada":
+        console.log ("Não desanime! Vamos tentar outra vez?");
+        break
+}
