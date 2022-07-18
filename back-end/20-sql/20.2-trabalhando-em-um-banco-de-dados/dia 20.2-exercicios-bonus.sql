ee46ee2176ffe9021d2b69e07cf216506b491b3f
Augusto Varandas <augustovarandas21@gmail.com>
2022-07-18 13:46:53 -0300

    Exercícios do dia 20.2

---
+++ back-end/20-sql/20.2-trabalhando-em-um-banco-de-dados/dia 20.2-exercicios-bonus.sql
@@ -0,0 +1,23 @@
+-- Escreva uma query para exibir a peça e o preço de tudo que é provido pela empresa RBT.
+SELECT piece, price FROM Provides
+WHERE provider = 'RBT';
+
+-- Escreve uma query para exibir todas as informações das cinco peças com os maiores preços.
+SELECT piece, price FROM Provides
+ORDER BY price DESC
+LIMIT 5;
+
+-- Escreva uma query para exibir o nome das empresas e preço das peças com os quatro maiores preços, começando a lista a partir do 3º item.
+SELECT DISTINCT provider, price FROM Provides
+ORDER BY price DESC
+LIMIT 4
+OFFSET 2;
+
+-- Escreva uma query para exibir todas as informações das peças que são providas pela empresa HAL. Ordene o resultado pelos preços das peças de forma decrescente.
+SELECT * FROM Provides
+WHERE provider = 'HAL'
+ORDER BY price DESC;
+
+-- Escreva uma query para exibir por quantas empresas a peça 1 é provida.
+SELECT COUNT(provider) FROM Provides
+WHERE piece = 1;
